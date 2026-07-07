4501a34654c4be90e257cc586bf33483f7fb6121
gamer_3001 <link59147@gmail.com>
2026-07-07 19:28:46 +0200

    Correction du VPK

--- ScoreManager.gd
+++ ScoreManager.gd
@@ -2,6 +2,7 @@ extends Node
 
 var high_score_campaign = 0
 var high_score_infinite = 0
+var high_score_survival = 0
 
 const SAVE_PATH = "user://savegame.save"
 
@@ -10,15 +11,21 @@ func _ready():
 
 func save_game():
     var file = File.new()
-    file.open(SAVE_PATH, File.WRITE)
-    file.store_var(high_score_campaign)
-    file.store_var(high_score_infinite)
-    file.close()
+    var err = file.open(SAVE_PATH, File.WRITE)
+    if err == OK:
+        file.store_var(high_score_campaign)
+        file.store_var(high_score_infinite)
+        file.store_var(high_score_survival)
+        file.close()
 
 func load_game():
     var file = File.new()
     if file.file_exists(SAVE_PATH):
-        file.open(SAVE_PATH, File.READ)
-        high_score_campaign = file.get_var()
-        high_score_infinite = file.get_var()
-        file.close()
+        var err = file.open(SAVE_PATH, File.READ)
+        if err == OK:
+            # On vérifie la taille pour éviter de lire des données corrompues si le fichier est vide
+            if file.get_len() > 0:
+                high_score_campaign = file.get_var()
+                high_score_infinite = file.get_var()
+                high_score_survival = file.get_var()
+            file.close()
